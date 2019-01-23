1d5e79d12bdfec83598ffcdfde159151b30dabc1
yangleir <yangleir@qq.com>
2019-01-23 16:05:55 +0800

    turn to Python3 from Python2

--- alti_read/t1.py
+++ alti_read/t1.py
@@ -6,22 +6,22 @@ import numpy as np
 import sys
 
 file_path = "/home/yl/Documents/python3_stu/netCDF4/data/deal_nc.nc"
-print file_path
+print (file_path)
 nc = Dataset(file_path)
-print nc.variables.keys()
+print (nc.variables.keys())
 
 lat = nc.variables['latitude'][:]
 lon = nc.variables['longitude'][:]
 tim = nc.variables['time'][:]
 vlz = nc.variables['z'][:]
 
-print np.shape(lon)
-print np.shape(lat)
-print np.shape(tim)
-print np.shape(vlz)
+print (np.shape(lon))
+print (np.shape(lat))
+print (np.shape(tim))
+print (np.shape(vlz))
 # print lat
 # latinfo = nc.variables['latitude']
 vlzinfo = nc.variables['u']
-print vlzinfo
+print (vlzinfo)
 #np.savetxt("tt.npy", (lon.data), fmt="%10.5f", delimiter=",")
 
